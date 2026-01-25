bce9bab37c9aeeff7612bb90c5ff1d50169e829f
Felix Mayer <FelixM@yer.tools>
2026-01-26 00:43:58 +0100

    Add pwm dimming for colors

--- Sipeed-Tang-Primer-20K/led_matrix/src/led_matrix.v
+++ Sipeed-Tang-Primer-20K/led_matrix/src/led_matrix.v
@@ -1,3 +1,17 @@
+module color_for(
+    input       [5:0]    x_pos,
+    input       [5:0]    y_pos,
+    output reg  [7:0]    red,
+    output reg  [7:0]    green,
+    output reg  [7:0]    blue
+);
+    always @(*) begin
+        red <= x_pos + y_pos + y_pos + y_pos;
+        green <= 0;
+        blue <= 0;
+    end
+endmodule
+
 module led_matrix(
     input           clk_27MHz,
 
@@ -15,14 +29,36 @@ module led_matrix(
     reg [2:0]   state;
 
     reg [5:0]   column;
+    reg [7:0]   color_depth;
 
     // Since we are showing the last row, while we are filling the next row, current_row is row + 1
     reg [4:0]   current_row;
 
-    reg [2:0]   color;
-    reg [6:0]   width;
+    wire    [7:0]   red1;
+    wire    [7:0]   green1;
+    wire    [7:0]   blue1;
+    wire    [7:0]   red2;
+    wire    [7:0]   green2;
+    wire    [7:0]   blue2;
 
-    always @(posedge clk_div[3]) begin
+    color_for(
+        .x_pos(column),
+        .y_pos(current_row + 6'd0),
+        .red(red1),
+        .green(green1),
+        .blue(blue1)
+    );
+
+
+    color_for(
+        .x_pos(column),
+        .y_pos(current_row + 6'd32),
+        .red(red2),
+        .green(green2),
+        .blue(blue2)
+    );
+
+    always @(posedge clk_27MHz) begin
         case (state)
             // This is a soft reset state to set get everything into an knowing state
             0: begin 
@@ -36,14 +72,8 @@ module led_matrix(
                 oe    <= 0; // Show the last row
                 latch <= 0;
 
-                column <= 0;
-
-                if (width == 0) begin
-                    if (color == 7) color <= 1;
-                    else color  <= color + 1;
-                end
-
-                width <= width + 1;
+                column      <= 0;
+                color_depth <= 0;
                 
                 state <= 1;
             end
@@ -52,13 +82,20 @@ module led_matrix(
             1: begin
                 clk <= 0;
     
-                if ((width[6] == 0 && column <= width[5:0]) || (width[6] == 1 && column >= width[5:0])) begin
-                    rgb1 <= 0;
-                    rgb2 <= color;
-                end else begin
-                    rgb1 <= color;
-                    rgb2 <= 0;
-                end
+                if (red1 > color_depth) rgb1[0] <= 1;
+                else rgb1[0] <= 0;
+                if (green1 > color_depth) rgb1[1] <= 1;
+                else rgb1[1] <= 0;
+                if (blue1 > color_depth) rgb1[2] <= 1;
+                else rgb1[2] <= 0;
+                
+
+                if (red2 > color_depth) rgb2[0] <= 1;
+                else rgb2[0] <= 0;
+                if (green2 > color_depth) rgb2[1] <= 1;
+                else rgb2[1] <= 0;
+                if (blue2 > color_depth) rgb2[2] <= 1;
+                else rgb2[2] <= 0;
                 
 
                 column <= column + 1;
@@ -98,8 +135,19 @@ module led_matrix(
                 latch <= 0;
                 oe    <= 0;
 
-                if (current_row == 0) state <= 0;
+                if (current_row == 0) state <= 6;
                 else state <= 1;
+            end
+
+            // Increment color depth
+            6: begin
+                if (color_depth == 254) begin
+                    color_depth <= 0;
+                    state <= 0;
+                end else begin
+                    color_depth <= color_depth + 1;
+                    state <= 1;
+                end
             end
 
             default: state <= 0; // Soft reset
@@ -107,3 +155,4 @@ module led_matrix(
     end
 
 endmodule
+
